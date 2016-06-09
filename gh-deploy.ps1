44c426aed1a0147c20815b7485ed88d77ff5264e
Martin Evans <martindevans@gmail.com>
2016-06-09 18:05:31 +0100

    Updated script to check that branch change succeeded
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- gh-deploy.ps1
+++ gh-deploy.ps1
@@ -1,12 +1,15 @@
 Try
 {
     $current_branch = git rev-parse --abbrev-ref HEAD
-    git checkout gh-deploy
-    gulp
-    git add dist -f
-    git commit -m "deploying..."
-    git subtree push --prefix dist origin gh-pages
-    Remove-Item dist
+    $ckerr = git checkout gh-deploy
+    if ($?)
+    {
+        gulp
+        git add dist -f
+        git commit -m "deploying..."
+        git subtree push --prefix dist origin gh-pages
+        Remove-Item dist
+    }
 }
 Finally
 {
