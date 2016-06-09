41ea27c14349cf6828cfe70fa82256a21954f095
Martin Evans <martindevans@gmail.com>
2016-06-09 18:24:26 +0100

    Updated script to not bother messing with alternative branches
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- gh-deploy.ps1
+++ gh-deploy.ps1
@@ -1,19 +1,7 @@
-Try
-{
-    $current_branch = git rev-parse --abbrev-ref HEAD
-    $ckerr = git checkout gh-deploy
-    if ($?)
-    {
-        git reset master --hard
-        gulp
-        git add dist -f
-        git commit -m "deploying..."
-        $magic = git subtree split --prefix dist gh-pages
-        git subtree push origin $magic:gh-pages --force
-        Remove-Item dist -Recurse
-    }
-}
-Finally
-{
-    git checkout $current_branch
-}
+gulp
+git add dist -f
+git commit -m "deploying"
+git subtree push --prefix dist origin gh-pages
+Remove-Item dist -Recurse
+git commit -m "post deploying"
+
