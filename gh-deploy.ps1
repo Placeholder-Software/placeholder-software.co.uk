60c3b2d728189ee80ee672556fe62d1ccd1d6d95
Martin Evans <martindevans@gmail.com>
2016-06-09 17:57:51 +0100

    Completed header
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ gh-deploy.ps1
@@ -0,0 +1,13 @@
+Try
+{
+    $current_branch = git rev-parse --abbrev-ref HEAD
+    git checkout gh-deploy
+    gulp
+    git add dist -f
+    git commit -m "deploying..."
+    git subtree push --prefix dist origin gh-pages
+}
+Finally
+{
+    git checkout $current_branch
+}
