80e37a562ce43d84846ba99973c5aafcc122f0be
Martin Evans <martindevans@gmail.com>
2016-06-09 18:41:05 +0100

    Merging master into deploy branch using theirs merge strategy
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- gh-deploy.ps1
+++ gh-deploy.ps1
@@ -1,7 +1,23 @@
-gulp
-git add dist -f
-git commit -m "deploying"
-git subtree push --prefix dist origin gh-pages
-Remove-Item dist -Recurse
-git commit -m "post deploying"
+Try
+{
+    #Get the current branch, so we can switch back to it later
+    $current_branch = git rev-parse --abbrev-ref HEAD
+    
+    #Switch to the deploy branch (check for error!)
+    git checkout gh-deploy
+    if ($?)
+    {
+        git merge -X theirs master
+        gulp
+        git add dist -f
+        git commit -m "deploying..."
+        git subtree push --prefix dist origin gh-pages
+        Remove-Item dist -Recurse
+        git commit -m "deployed"
+    }
+}
+Finally
+{
+    git checkout $current_branch
+}
 
