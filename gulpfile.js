4411ca9f9c4d313db74d72976250e38250725629
Martin Evans <martindevans@gmail.com>
2018-03-29 13:50:54 +0100

    Added gulp-multi-process to run some tasks in parallel

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,22 @@ const $ = require('gulp-load-plugins')();
 
 const config = require('./build/config.js');
 
+//Create the basic tasks
 gulp.task('images', require("./build/tasks/images.js")($, gulp, config));
 gulp.task('fonts', require("./build/tasks/fonts.js")($, gulp, config));
 gulp.task('copy', require("./build/tasks/copy.js")($, gulp, config));
 gulp.task('styles', require("./build/tasks/styles.js")($, gulp, config));
 gulp.task('scripts', require("./build/tasks/scripts.js")($, gulp, config));
-gulp.task('html', ['styles', 'scripts'], require("./build/tasks/html.js")($, gulp, config));
 
+//Create a pre-html task which runs the HTML task depenedencies in parallel. Then depend on that in the HTML task
+gulp.task('pre-html', (cb) => $.multiProcess(['styles', 'scripts'], cb, true));
+gulp.task('html', ['pre-html'], require("./build/tasks/html.js")($, gulp, config));
+
+//Default action runs the build after cleaning the output directory. Once completed print out summary information about the build result.
+gulp.task('default', ['clean', 'build'], () => gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true })));
+
+//Clean deletes the output and .tmp directories
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
-gulp.task('build', ['html', 'images', 'fonts', 'copy'], () => gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true })));
-gulp.task('default', ['clean'], () => gulp.start('build'));
+
+//Run all the work of the build (each task in parallel)
+gulp.task('build', (cb) => $.multiProcess(['html', 'images', 'fonts', 'copy'], cb, true));
