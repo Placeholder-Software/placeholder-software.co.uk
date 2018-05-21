9bfb97fa543a0746c84647f3998d920ec53fa6a6
Martin Evans <martindevans@gmail.com>
2018-05-21 20:24:14 +0100

    Added image slider to demonstrate the change

--- app/scripts/wetstuff/gallery.js
+++ app/scripts/wetstuff/gallery.js
@@ -1,7 +1,7 @@
 $(function() {
 
     $("[data-carousel-video-container*='true']")
-        .on('slide.bs.carousel', function (e) {
+        .on("slide.bs.carousel", function (e) {
 
             //Find all videos in carousel and unset their src attribute
             $(this)
@@ -20,4 +20,60 @@ $(function() {
                     console.log("Loaded video");
                 });
         });
+
+    //make the .cd-handle element draggable and modify .cd-resize-img width according to its position
+    $('.cd-image-container').each(function(){
+        var actual = $(this);
+        gallery_drags(actual.find('.cd-handle'), actual.find('.cd-resize-img'), actual);
+    });
+        
 });
+
+//draggable funtionality - credits to http://css-tricks.com/snippets/jquery/draggable-without-jquery-ui/
+function gallery_drags(dragElement, resizeElement, container)
+{
+    dragElement.on("mousedown vmousedown", function(e) {
+        dragElement.addClass('draggable');
+        resizeElement.addClass('resizable');
+    
+        var dragWidth = dragElement.outerWidth();
+        var xPosition = dragElement.offset().left + dragWidth - e.pageX;
+        var containerOffset = container.offset().left;
+        var containerWidth = container.outerWidth();
+        var minLeft = containerOffset + 40;
+        var maxLeft = containerOffset + containerWidth - dragWidth - 40;
+            
+        dragElement.parents().on("mousemove vmousemove", function(e) {
+            leftValue = e.pageX + xPosition - dragWidth;
+                
+            //constrain the draggable element to move inside its container
+            if(leftValue < minLeft ) {
+                leftValue = minLeft;
+            } else if ( leftValue > maxLeft) {
+                leftValue = maxLeft;
+            }
+    
+            widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
+    
+            $('.draggable').css('left', widthValue).on("mouseup vmouseup", function() {
+                $(this).removeClass('draggable');
+                resizeElement.removeClass('resizable');
+            });
+    
+            $('.resizable').css('width', widthValue); 
+    
+            //function to upadate images label visibility here
+            // ...
+    
+        }).on("mouseup vmouseup", function(e){
+            dragElement.removeClass('draggable');
+            resizeElement.removeClass('resizable');
+        });
+
+        e.preventDefault();
+
+    }).on("mouseup vmouseup", function(e) {
+       dragElement.removeClass('draggable');
+       resizeElement.removeClass('resizable');
+    });
+ }
