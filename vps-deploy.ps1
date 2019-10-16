7909cab939f9258c7ee67fb4ada7f6396a959577
Martin Evans <martindevans@gmail.com>
2019-10-16 16:03:26 +0100

    using alternative URL for deployment

--- vps-deploy.ps1
+++ vps-deploy.ps1
@@ -1,3 +1,3 @@
 gulp default --base_path="//placeholder-software.co.uk"
-bash -c "scp -rp dist martindevans@placeholder-software.co.uk:/var/www/placeholder-software.co.uk_new"
-bash -c 'ssh martindevans@placeholder-software.co.uk "/var/www/update-placeholder-software.co.uk.sh"'
+bash -c "scp -rp dist martindevans@mumble.placeholder-software.co.uk:/var/www/placeholder-software.co.uk_new"
+bash -c 'ssh martindevans@mumble.placeholder-software.co.uk "/var/www/update-placeholder-software.co.uk.sh"'
